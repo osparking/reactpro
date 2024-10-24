68c72987582c758a8be1a9fb26dbb98183f89c54
osparking <jbpark03@gmail.com>
2024-10-24 14:27:42 +0900

    using function call in jsx

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,7 +1,10 @@
 const name = "항재";
 
+function showMsg() {
+    return "장관이다!";
+}
 function Hello() {
-  return <h1>안녕하십니까 - {name}</h1>;
+  return <h1>안녕하십니까 - {showMsg()}</h1>;
 }
 
 export default Hello;
