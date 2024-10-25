04f4df7209104fb407cc372a539b3e8abb5719f7
osparking <jbpark03@gmail.com>
2024-10-25 21:24:36 +0900

    destructuring parameters

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,9 +1,9 @@
-function Hello(props) {
+function Hello({person}) {
   return (
     <div>
       <h1>
-        안녕하십니까 - {props.person.name}- {props.person.message}-
-        {props.person.emoji}- {props.person.numbers}
+        안녕하십니까 - {person.name}- {person.message}-
+        {person.emoji}- {person.numbers}
       </h1>
     </div>
   );
