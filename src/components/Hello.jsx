54d352daed4d2385e529ca7cbe7c2d0047bdc8d3
osparking <jbpark03@gmail.com>
2024-10-25 11:17:31 +0900

    destructuring thru parameter passing

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,14 +1,9 @@
-const name = "항재";
-
-function showMsg() {
-  return "장관이다!";
-}
-function Hello(props) {
-  console.log(props);
-  const {name, message} = props;
+function Hello({ name, message }) {
   return (
     <div>
-      <h1>안녕하십니까 - {name} - {message}</h1>
+      <h1>
+        안녕하십니까 - {name} - {message}
+      </h1>
     </div>
   );
 }
