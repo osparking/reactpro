5772ee79d9dbe55f46af5e339091b24f93ea094a
osparking <jbpark03@gmail.com>
2024-10-26 12:15:40 +0900

    create array of fruit objects

--- src/components/Fruits.jsx
+++ src/components/Fruits.jsx
@@ -1,5 +1,11 @@
 export default function Fruits() {
-  const fruits = ["감", "배", "밤", "대추", "포도"];
+  const fruits = [
+    { name: "복숭아", price: 700, emoji: "🍑" },
+    { name: "사과", price: 1700, emoji: "🍎" },
+    { name: "망고", price: 3200, emoji: "🥭" },
+    { name: "바나나", price: 400, emoji: "🍌" },
+    { name: "파인애플", price: 3800, emoji: "🍍" },
+  ];
   return (
     <div>
       <ul>
