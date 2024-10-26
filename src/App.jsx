4d380de0439b197feb70bb11af64c6f0e2b4282c
osparking <jbpark03@gmail.com>
2024-10-26 15:48:58 +0900

    use local bool const to select return statement

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import Hello from "./components/Hello";
 import Fruits from "./components/Fruits";
+import CondCompo from "./components/CondCompo";
 function App() {
   return (
     <div className="App">
-      <Fruits />
+      <CondCompo />
     </div>
   );
 }
--- src/components/CondCompo.jsx
+++ src/components/CondCompo.jsx
@@ -1,6 +1,16 @@
 export default function CondCompo() {
-    return <div>
+  const condition = true;
+  if (condition) {
+    return (
+      <div>
         <h3>이 성분은 조건이 참일 때 표출된다.</h3>
+      </div>
+    );
+  } else {
+    return (
+      <div>
         <h3>이 성분은 조건이 거짓일 때 표출된다.</h3>
-    </div>
+      </div>
+    );
+  }
 }
