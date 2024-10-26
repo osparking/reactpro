bf180642a47e35c6602570dbbc575b57110e52bb
osparking <jbpark03@gmail.com>
2024-10-26 09:54:10 +0900

    make each fruit name appear after a bullet

--- src/components/Fruits.jsx
+++ src/components/Fruits.jsx
@@ -2,9 +2,11 @@ export default function Fruits() {
   const fruits = ["감", "배", "밤", "대추", "포도"];
   return (
     <div>
-      {fruits.map((fruit) => (
-        <h3>{fruit}</h3>
-      ))}
+      <ul>
+        {fruits.map((fruit) => (
+          <li>{fruit}</li>
+        ))}
+      </ul>
     </div>
   );
 }
