3dd096a68f4c75c02b17f803a6736d36c0d0e8fa
osparking <jbpark03@gmail.com>
2024-10-26 20:37:17 +0900

    make clean code using ternery operator

--- src/components/CondCompo.jsx
+++ src/components/CondCompo.jsx
@@ -2,13 +2,11 @@ import CompoFalse from "./CompoFalse";
 import CompoTrue from "./CompoTrue";
 
 export default function CondCompo() {
-  let message1 = <h3>이것은 하나의 전갈이다.</h3>;
-  let message2 = <h3>이것은 또 다른 전갈이다.</h3>;
-  const condition = false;
+  const condition = true;
 
-  if (condition) {
-    return message1;
-  } else {
-    return message2;
-  }
+  return condition ? (
+    <h3>콧노래를 흥얼흥얼부른다."</h3>
+  ) : (
+    <h3>"얼굴에 인상을 쓴다."</h3>
+  );
 }
