4eddc2b60cfe224b212790d32abe5598b3436c37
osparking <jbpark03@gmail.com>
2024-10-26 19:22:38 +0900

    just demonstrates html element variable

--- src/components/CondCompo.jsx
+++ src/components/CondCompo.jsx
@@ -2,10 +2,13 @@ import CompoFalse from "./CompoFalse";
 import CompoTrue from "./CompoTrue";
 
 export default function CondCompo() {
+  let message1 = <h3>이것은 하나의 전갈이다.</h3>;
+  let message2 = <h3>이것은 또 다른 전갈이다.</h3>;
   const condition = false;
+
   if (condition) {
-    return <CompoTrue />;
+    return message1;
   } else {
-    return <CompoFalse />;
+    return message2;
   }
 }
