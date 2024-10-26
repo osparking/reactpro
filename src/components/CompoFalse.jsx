ccee5735728a4fee6c44425e9c1e9bfa01baa9e4
osparking <jbpark03@gmail.com>
2024-10-26 18:13:38 +0900

    each compo' has a single return statement

---
+++ src/components/CompoFalse.jsx
@@ -0,0 +1,7 @@
+export default function CompoFalse() {
+  return (
+    <div>
+      <h3>이 성분은 조건이 거짓일 때 표출된다.</h3>
+    </div>
+  );
+}
---
+++ src/components/CompoTrue.jsx
@@ -0,0 +1,7 @@
+export default function CompoTrue() {
+  return (
+    <div>
+      <h3>이 성분은 조건이 참일 때 표출된다.</h3>
+    </div>
+  );
+}
