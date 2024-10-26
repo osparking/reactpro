d8f16399994757434193039028f6bf7912371521
osparking <jbpark03@gmail.com>
2024-10-26 09:44:49 +0900

    use map function to handle each fruit

--- src/components/Fruits.jsx
+++ src/components/Fruits.jsx
@@ -1,4 +1,10 @@
 export default function Fruits() {
-    const fruits = ["감", "배", "밤", "대추", "포도"];
-    return <div>{fruits}</div>;
+  const fruits = ["감", "배", "밤", "대추", "포도"];
+  return (
+    <div>
+      {fruits.map((fruit) => (
+        <h3>{fruit}</h3>
+      ))}
+    </div>
+  );
 }
