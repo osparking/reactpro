1a4ab029f59db123025e53580572f67594185caa
osparking <jbpark03@gmail.com>
2024-10-27 21:58:30 +0900

    define basic onChange event handler

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,11 @@
 export default function Form() {
+  function handleChange() {
+    console.log("name 입력 상자 값 변화됨!");
+  }
   return (
     <div>
       <form>
-        <input type="text" value="name" />
+        <input onChange={handleChange} type="text" value="name" />
       </form>
     </div>
   );
