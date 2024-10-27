7471e85bb22eb941c455b6307bbe38918e83c704
osparking <jbpark03@gmail.com>
2024-10-27 20:52:05 +0900

    create a dummy Form compo'

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import Counter from "./components/Counter";
+import Form from "./components/Form";
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <Form />
     </div>
   );
 }
