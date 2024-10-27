98405ac173f2fbe8d47dc91821d1089cfc113db7
osparking <jbpark03@gmail.com>
2024-10-27 15:49:38 +0900

    add decrease button

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -5,11 +5,17 @@ export default function Counter() {
   function incByOne() {
     setCount(count + 1);
   }
+  function decByOne() {
+    setCount(count - 1);
+  }
   return (
     <div>
       <h3>계수기: {count}</h3>
       <button onClick={incByOne}>
         1 증가
+      </button>
+      <button onClick={decByOne}>
+        1 감소
       </button>
     </div>
   );
