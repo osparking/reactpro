d1508dfabfb09d98e251e6c9ad96db06496608c1
osparking <jbpark03@gmail.com>
2024-10-27 15:45:09 +0900

    add inc button and function

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,9 +2,15 @@ import { useState } from "react";
 
 export default function Counter() {
   const [count, setCount] = useState(0);
+  function incByOne() {
+    setCount(count + 1);
+  }
   return (
     <div>
       <h3>계수기: {count}</h3>
+      <button onClick={incByOne}>
+        1 증가
+      </button>
     </div>
   );
 }
