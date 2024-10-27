5f89794750c5706adc0eb3684902cde9177acbc8
osparking <jbpark03@gmail.com>
2024-10-28 06:32:57 +0900

    define state variable for name field

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,15 @@
+import { useState } from "react"
+
 export default function Form() {
+  const [name, setName] = useState("");
+
   function handleChange() {
     console.log("name 입력 상자 값 변화됨!");
   }
   return (
     <div>
       <form>
-        <input onChange={handleChange} type="text" value="name" />
+        <input onChange={handleChange} type="text" value={name} />
       </form>
     </div>
   );
