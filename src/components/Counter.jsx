4d9eb50cdd48ad14f323c81b1b7106aac81cd565
osparking <jbpark03@gmail.com>
2024-10-27 18:31:54 +0900

    employ delta state variable for the counter

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,21 +2,27 @@ import { useState } from "react";
 
 export default function Counter() {
   const [count, setCount] = useState(0);
+  const [delta, setDelta] = useState(1);
   function incByOne() {
-    setCount(count + 1);
+    setCount(count + delta);
   }
   function decByOne() {
-    setCount(count - 1);
+    setCount(count - delta);
+  }
+  function incDelta() {
+    setDelta(delta + 1);
+  }
+  function decDelta() {
+    setDelta(delta - 1);
   }
   return (
     <div>
       <h3>계수기: {count}</h3>
-      <button onClick={incByOne}>
-        1 증가
-      </button>
-      <button onClick={decByOne}>
-        1 감소
-      </button>
+      <button onClick={incByOne}>증가</button>
+      <button onClick={decByOne}>감소</button>
+      <h3>델타: {delta}</h3>
+      <button onClick={incDelta}>증가</button>
+      <button onClick={decDelta}>감소</button>
     </div>
   );
 }
