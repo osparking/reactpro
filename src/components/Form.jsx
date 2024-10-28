2ad4e92cc679b6ebb8dde8bb24c3a5caa4520823
osparking <jbpark03@gmail.com>
2024-10-28 12:12:46 +0900

    use target value as lastName of name object

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,14 +2,14 @@ import { useState } from "react";
 
 export default function Form() {
   const [name, setName] = useState({ lastName: "", firstName: "" });
-
+  console.log(name.lastName);
   return (
     <div>
       <form>
         <input
-          onChange={(e) => setName(e.target.value)}
+          onChange={(e) => setName({ lastName: e.target.value, firstName: "" })}
           type="text"
-          value={name}
+          value={name.lastName}
         />
       </form>
     </div>
