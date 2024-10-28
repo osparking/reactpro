fb8bffcee68b72e83b4d089b63ecb1f3d58f4d4c
osparking <jbpark03@gmail.com>
2024-10-28 10:02:20 +0900

    final version of on change event handling

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,13 +3,14 @@ import { useState } from "react";
 export default function Form() {
   const [name, setName] = useState("");
 
-  function handleChange(e) {
-    setName(e.target.value);
-  }
   return (
     <div>
       <form>
-        <input onChange={(e) => handleChange(e)} type="text" value={name} />
+        <input
+          onChange={(e) => setName(e.target.value)}
+          type="text"
+          value={name}
+        />
       </form>
     </div>
   );
